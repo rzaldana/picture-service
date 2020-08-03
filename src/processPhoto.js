15db571d8eb6beecb34776d7965833a66f3b827b
Raul Zaldana <raularmandozc12@gmail.com>
2020-08-02 20:08:40 -0400

    added link to reference code fro processPhoto

--- src/processPhoto.js
+++ src/processPhoto.js
@@ -1,6 +1,8 @@
 const AWS = require('aws-sdk');
 const sharp = require('sharp');
 
+// code based on https://docs.aws.amazon.com/lambda/latest/dg/with-s3-example.html
+
 // make a new s3 client
 const s3 = new AWS.s3();
 
@@ -9,7 +11,7 @@ async function processPhoto(event, context) {
     console.log(JSON.stringify(event));
 
     // Lambda URI encodes object keys so we need to decode them
-    const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+    const srcKey = decodeURIComponent(event.Records[0].s3.object.key);
     const dstBucket = srcBucket + "-resized"; // need to edit this
     const dstKey = "resized-" + srcKey; // need to edit this
 
