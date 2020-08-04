2d90af8803019d1564564d93017d61b568beb69b
Raul Zaldana <raularmandozc12@gmail.com>
2020-08-03 22:48:44 -0400

    added getPhotos function and http endpoint

---
+++ src/getPhotos.js
@@ -0,0 +1,23 @@
+const AWS = require('aws-sdk');
+const createError = require('http-errors');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getPhotos(event, context) {
+    let photos;
+
+    try {
+        const result = await dynamodb.scan({ TableName: 'PhotoTable' }).promise();
+        photos = result.Items;
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
+
+    return ({
+        statusCode: 200,
+        body: JSON.stringify(photos),
+    });
+}
+
+export const handler = getPhotos;
